1375353eb98bbebdf9d352e8257e0b0ecc64400d
liyuanwu <16619891743@163.com>
2023-03-08 00:24:53 +0800

    import github.com/go-playground/validator

--- context.go
+++ context.go
@@ -351,16 +351,62 @@ func checkParam(value reflect.Value, data any, decoder *json.Decoder) error {
 	return json.Unmarshal(b, data)
 }
 
+type SliceValidationError []error
+
+func (err SliceValidationError) Error() string {
+	n := len(err)
+	switch n {
+	case 0:
+		return ""
+	default:
+		var b strings.Builder
+		if err[0] != nil {
+			_, err := fmt.Fprintf(&b, "[%d]: %s", 0, err[0].Error())
+			if err != nil {
+				return ""
+			}
+		}
+		if n > 1 {
+			for i := 1; i < n; i++ {
+				if err[i] != nil {
+					b.WriteString("\n")
+					_, err := fmt.Fprintf(&b, "[%d]: %s", i, err[i].Error())
+					if err != nil {
+						return ""
+					}
+				}
+			}
+		}
+		return b.String()
+	}
+}
+
 func validate(data any) error {
+	if data == nil {
+		return nil
+	}
 	v := reflect.ValueOf(data)
-	elem := v.Elem().Interface()
-	//todo
-	log.Println(v, v.Kind(), v.Type(), elem, v.Type().Elem())
 	switch v.Kind() {
 	case reflect.Array, reflect.Slice:
+		count := v.Len()
+		errs := make(SliceValidationError, 0)
+		for i := 0; i < count; i++ {
+			if err := validateStruct(v.Index(i).Interface()); err != nil {
+				errs = append(errs, err)
+			}
+		}
+		if len(errs) == 0 {
+			return nil
+		}
+		return errs
 	case reflect.Ptr:
+		return validate(v.Elem().Interface())
 	case reflect.Struct:
-		return validator.New().Struct(data)
+		return validateStruct(data)
 	}
 	return nil
+}
+
+func validateStruct(data any) error {
+	return validator.New().Struct(data)
 }
