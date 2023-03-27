851b7a6ba05b5e459467803a4846ee43506a868e
好好住-liyuanwu <liyuanwu@haohaozhu.com>
2023-03-27 19:01:05 +0800

    http rpc with error

--- rpc/http.go
+++ rpc/http.go
@@ -2,11 +2,17 @@ package rpc
 
 import (
 	"bufio"
+	"bytes"
+	"encoding/json"
 	"errors"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"net/url"
 	"reflect"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -32,16 +38,47 @@ func NewHttpClient() *MsHttpClient {
 	}
 }
 
-func (c *MsHttpClient) Get(url string) ([]byte, error) {
-
+func (c *MsHttpClient) Get(url string, args map[string]any) ([]byte, error) {
+	if args != nil && len(args) > 0 {
+		url = url + "?" + c.toValues(args)
+	}
+	log.Println(url)
 	request, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
 		return nil, err
 	}
-	return c.responseHandle(request)
+	return c.handleResponse(request)
 }
 
-func (c *MsHttpClient) responseHandle(request *http.Request) ([]byte, error) {
+func (c *MsHttpClient) PostForm(url string, args map[string]any) ([]byte, error) {
+	req, err := http.NewRequest("POST", url, strings.NewReader(c.toValues(args)))
+	if err != nil {
+		return nil, err
+	}
+	return c.handleResponse(req)
+}
+
+func (c *MsHttpClient) PostJson(url string, args map[string]any) ([]byte, error) {
+	jsonStr, _ := json.Marshal(args)
+	req, err := http.NewRequest("POST", url, bytes.NewReader(jsonStr))
+	if err != nil {
+		return nil, err
+	}
+	return c.handleResponse(req)
+}
+
+func (c *MsHttpClient) toValues(args map[string]any) string {
+	if args != nil && len(args) > 0 {
+		params := url.Values{}
+		for k, v := range args {
+			params.Set(k, fmt.Sprintf("%v", v))
+		}
+		return params.Encode()
+	}
+	return ""
+}
+
+func (c *MsHttpClient) handleResponse(request *http.Request) ([]byte, error) {
 	response, err := c.client.Do(request)
 	if err != nil {
 		return nil, err
@@ -76,9 +113,39 @@ func (c *MsHttpClient) responseHandle(request *http.Request) ([]byte, error) {
 }
 
 type HttpConfig struct {
-	Host string
-	Port int
+	Protocol string
+	Host     string
+	Port     int
+	Ssl      bool
 }
+
+const (
+	HTTP  = "HTTP"
+	HTTP2 = "HTTP2"
+	TCP   = "TCP"
+)
+
+const (
+	GET      = "GET"
+	POSTForm = "POST_FORM"
+	POSTJson = "POST_JSON"
+)
+
+func (c HttpConfig) Url() string {
+	if c.Protocol == "" {
+		c.Protocol = HTTP
+	}
+	switch c.Protocol {
+	case HTTP, HTTP2:
+		prefix := "http://"
+		if c.Ssl {
+			prefix = "https://"
+		}
+		return prefix + c.Host + ":" + strconv.FormatInt(int64(c.Port), 10)
+	}
+	return ""
+}
+
 type MsService interface {
 	Env() HttpConfig
 }
@@ -96,6 +163,43 @@ func (c *MsHttpClient) Do(service string, method string) MsService {
 	t := reflect.TypeOf(msService)
 	v := reflect.ValueOf(msService)
 	if t.Kind() != reflect.Pointer {
-
+		panic(errors.New("service must be pointer"))
 	}
+	tVar := t.Elem()
+	vVar := v.Elem()
+	fieldFind := -1
+	for i := 0; i < tVar.NumField(); i++ {
+		name := tVar.Field(i).Name
+		if name == method {
+			fieldFind = i
+			break
+		}
+	}
+	if fieldFind == -1 {
+		panic(errors.New("method not found"))
+	}
+	requestPath := tVar.Field(fieldFind).Tag.Get("msrpc")
+	if requestPath == "" {
+		panic(errors.New("msrpc tag not exist"))
+	}
+	split := strings.Split(requestPath, ",")
+	mt := split[0]
+	path := split[1]
+	co := msService.Env()
+	prefix := co.Url()
+	f := func(args map[string]any) ([]byte, error) {
+		if mt == GET {
+			return c.Get(prefix+path, args)
+		}
+		if mt == POSTForm {
+			return c.PostForm(prefix+path, args)
+		}
+		if mt == POSTJson {
+			return c.PostJson(prefix+path, args)
+		}
+		return nil, nil
+	}
+	value := reflect.ValueOf(f)
+	vVar.Field(fieldFind).Set(value)
+	return msService
 }
